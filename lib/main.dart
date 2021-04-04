b59ae3ca30f9725d406a23e0024b58477496e0f6
Daniel <dany.torrealva7x@gmail.com>
2021-04-03 20:00:12 -0500

    main

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:appprueba/tareas/cy_tarea.dart';
+import 'package:appprueba/tareas/sebas_tarea.dart';
+import 'package:appprueba/tareas/daniel_tarea.dart';
+import 'package:flutter/services.dart';
+
+class MyApp extends StatelessWidget {
+  final bool showBanner;
+
+  MyApp({this.showBanner});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Mi aplicación',
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(),
+              Container(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
